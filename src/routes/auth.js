a7a5e7063a5beff53a5993114b4fa821fb309d36
Claire Kodia <claire.kodia@gmail.com>
2020-05-11 17:07:04 +0200

    Authentification from database

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
 
+const connection = require("../helper/db.js")
 const jwtSecret = require("../../jwtSecret.js")
 
 const Router = express.Router()
@@ -12,20 +13,28 @@ Router.get("/", (req, res) => {
 Router.post("/login",(req, res) => {
     console.log("req.body", req.body)
 
-    if(
-        req.body.username === 'pinto' &&
-        req.body.password === 'coco'
+    const sql = "SELECT username, password FROM users WHERE username = ? AND password = ?"
+    const values = [
+        req.body.username,
+        req.body.password
+    ]
 
-    ){
-    const tokenUserInfo = {
-        username : req.body.username,
-        status : "Chicken Lord"
-    } 
-    const token = jwt.sign(tokenUserInfo,jwtSecret)
-    res.header("Access-Control-Expose-Headers" , "x-access-token")
-    res.set('x-access-token', token)
-    res.status(200).send('user connected')
-}
+    connection.query(sql, values, (err, result) => {
+        if (err) throw err;
+        if (result == false) {
+            return res.status(200).send("The password or username is wrong"); /* explorer les status pour prendre celui qui correspond sont bons */
+        } else {
+            const tokenUserInfo = {
+                username: req.body.username, /* a recuperer de la base de donnée */
+                status: "chicken lord" /* a recuperer de la base de donnée */
+            }
+            const token = jwt.sign(tokenUserInfo, jwtSecret)
+            res.header("Access-Control-Expose-Headers", "x-access-token")
+            res.set("x-access-token", token)
+    
+            return res.status(200).send("user is connected")
+        }
+    })
 })
 
 module.exports = Router
