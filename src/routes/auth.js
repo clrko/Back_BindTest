d8752653bc13b44a1976755880d89364787d6342
Claire Kodia <claire.kodia@gmail.com>
2020-05-11 16:00:03 +0200

    Added the token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const jwt = require("jsonwebtoken")
+
+const jwtSecret = require("../../jwtSecret.js")
 
 const Router = express.Router()
 
@@ -8,7 +11,23 @@ Router.get("/", (req, res) => {
 
 Router.post("/login",(req, res) => {
     console.log("req.body", req.body)
-    res.send("Je suis dans la route /auth/login")
+
+    if(
+        req.body.username === 'pinto' &&
+        req.body.password === 'coco'
+
+    ){
+    const tokenUserInfo = {
+        username : req.body.username,
+        status : "Chicken Lord"
+    } 
+    const token = jwt.sign(tokenUserInfo,jwtSecret)
+    console.log("voici le token", token)
+    /* res.header("Access-Control-Expose-Headers" , "x-acess-token")
+    res.set('x-acess-token', token) */
+    res.status(200).send('user connected')
+
+}
 })
 
 module.exports = Router
