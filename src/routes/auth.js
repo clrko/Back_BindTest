7adee896e9417ec8f65f3020cc673a76dd74f8a3
Claire Kodia <claire.kodia@gmail.com>
2020-05-12 17:38:28 +0200

    Finalized the requests and responses with the database and the front

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
 
 const connection = require("../helper/db.js")
 const jwtSecret = require("../../jwtSecret.js")
@@ -20,40 +21,33 @@ Router.post("/login",(req, res) => {
 
     connection.query(sql, values, (err, result) => {
         if (err) throw err;
-        if (!result) {
-            return res.status(200).send("The password or username is wrong"); /* TODO explorer les status pour prendre celui qui correspond sont bons */
-        } else {
-           /*  bcrypt.compare(req.body.password, result[]de la DB, function(err, result) {
-                // result == true
-            });
 
-            if  */
+        console.log("result est", result[0].id)
 
-            console.log(result)
-            const tokenUserInfo = {
-                id: result[0].id,
-                username: result[0].username,
+        const saltRounds = 10;
+        const myPlaintextPassword = req.body.password;
+        const id = result[0].id;
+        const username = result[0].username;
+
+        bcrypt.compare(myPlaintextPassword, result[0].password, function(err, result) {
+            if (result) {
+                const tokenUserInfo = {
+                    id: id,
+                    username: username,
+                }
+                console.log("user token info est", tokenUserInfo)
+                const token = jwt.sign(tokenUserInfo, jwtSecret)
+                console.log("user token est", token)
+    
+                res.header("Access-Control-Expose-Headers", "x-access-token")
+                res.set("x-access-token", token)
+                return res.status(201).send("The user is connected")
+            } else {
+                return res.status(200).send("The password or username is wrong")
             }
-            console.log("user token info est", tokenUserInfo)
-            const token = jwt.sign(tokenUserInfo, jwtSecret)
-            console.log("user token est", token)
-
-            res.header("Access-Control-Expose-Headers", "x-access-token")
-            res.set("x-access-token", token)
-            return res.status(200).send("user is connected")
-        }
+        })
     })
 })
 
-Router.post("/test",(req, res) => {
-    console.log("req.body", req.body) /* dans le body il faudra mettre les informations, chansons, score etc. */
-    console.log("headers", req.headers)
-    const token = req.headers['x-access-token'] /* Puré revoir le cours - les tirets ou espaces ne marchent pas donc il faut utiliser la synthaxe avec les [] et '' */
-    console.log("token est", token)
-    const tokenData = jwt.verify(token, jwtSecret)
-    console.log("user data token", tokenData)
-    /* Gérer quand c'est pas bon */
-    /* mtn on peut utiliser lid pour sauvegarder */
-})
 
 module.exports = Router
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 const connection = require("../helper/db.js")
 
