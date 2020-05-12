b70087bd64c0e07229f57870f152e16df03c76fd
Claire Kodia <claire.kodia@gmail.com>
2020-05-13 00:07:22 +0200

    Trying to input the delete

--- src/routes/favorite.js
+++ src/routes/favorite.js
@@ -32,44 +32,42 @@ Router.post("/tracks", (req, res) => {
     console.log("headers", req.headers)
     
     const token = req.headers['x-access-token'] 
-    console.log("token est", token)
-    
     const tokenData = jwt.verify(token, jwtSecret, (err, decoded) => {
         if (err) throw err;
 
-        const sql = "SELECT (track_id) FROM users WHERE user_id = ?"
+        const sql = "INSERT INTO favorite (user_id, track_id) VALUES (?, ?)"
         const values = [
-            req.body.username,
+            decoded.id,
+            req.body.track_id,
+        ]
+        console.log("values are", values)
+        connection.query(sql, values, (err, result) => {
+            if (err) throw err;
+            return res.status(200).send("Added to favorite");
+        })
+    })
+})
+
+/* ou delete */
+Router.put("/tracks", (req, res) => {
+    const token = req.headers['x-access-token'] 
+
+    console.log("req.data", req.data) 
+    console.log("headers", req.headers)
+    const tokenData = jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) throw err;
+
+        const sql = "DELETE FROM favorite WHERE id = ? AND track_id = ?"
+        const values = [
+            decoded.id,
+            req.data.track_id,
         ]
         connection.query(sql, values, (err, result) => {
             if (err) throw err;
-
-            if (result.length === 0 ) {
-                const sql = "INSERT INTO favorite (user_id, track_id) VALUES (?, ?)"
-                const values = [
-                    decoded.id,
-                    req.body.track_id,
-                ]
-                console.log("values are", values)
-                connection.query(sql, values, (err, result) => {
-                    if (err) throw err;
-                    return res.status(200).send("Added to favorite");
-                })
-            } else {
-                const sql = "DELETE FROM favorite WHERE id = ? AND track_id = ?"
-                const values = [
-                    decoded.id,
-                    req.body.track_id,
-                ]
-                console.log("values are", values)
-                connection.query(sql, values, (err, result) => {
-                    if (err) throw err;
-                    return res.status(200).send("Removed favorite")
-                })
-            }
-        })
+            return res.status(200).send("Removed favorite")
+        })     
     })
-    console.log("user data token", tokenData)
 })
+
 
 module.exports = Router
