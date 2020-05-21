9b56c45ab5c0605c0e1b63b3019ff66711c87cf1
SebG-prog <sebastien-gruet@orange.fr>
2020-05-21 12:00:29 +0200

    hot fix to be able to play music genre with spaces and/or slashes

--- src/routes/ranking.js
+++ src/routes/ranking.js
@@ -6,8 +6,17 @@ const jwtSecret = require("../../jwtSecret.js")
 
 const Router = express.Router()
 
-Router.get("/", (req, res) => {
-    res.send("je suis dans la route /ranking")
+Router.get("/allscores", (req, res) => {
+    const token = req.headers['x-access-token']
+    const tokenData = jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) throw err;
+        const sql = "SELECT id, username, score, genre FROM score WHERE user_id = ?"
+        const values = [decoded.id]
+        connection.query(sql, values, (err, result) => {
+            if (err) throw err;
+            return res.status(200).send(result);
+        })
+    })
 })
 
 Router.get("/standard", (req, res) => {
@@ -19,8 +28,7 @@ Router.get("/standard", (req, res) => {
 })
 
 Router.get("/standard/:genre", (req, res) => {
-    console.log("REQ PARAMS", req.params.genre)
-    const genre = req.params.genre
+    const genre = req.params.genre.replace(/%20/g, ' ').replace(/%2F/g, '/')
     const sql = "SELECT * FROM score WHERE genre = ?"
     const values = [genre]
     connection.query(sql, values, (err, result) => {
@@ -30,8 +38,6 @@ Router.get("/standard/:genre", (req, res) => {
 })
 
 Router.put("/updateScore/:id", (req, res) => {
-    console.log("body", req.body)
-    console.log("params", req.params)
     const id = req.params.id
     const sql = `UPDATE score SET score = ? WHERE id = ?`
     const values = [
@@ -45,8 +51,6 @@ Router.put("/updateScore/:id", (req, res) => {
 })
 
 Router.post("/addScore", (req, res) => {
-    console.log("req.body", req.body)
-
     const token = req.headers['x-access-token']
     const tokenData = jwt.verify(token, jwtSecret, (err, decoded) => {
         if (err) throw err;
@@ -55,7 +59,7 @@ Router.post("/addScore", (req, res) => {
             decoded.id,
             req.body.username,
             req.body.score,
-            req.body.genre,
+            req.body.genre.replace(/%20/g, ' ').replace(/%2F/g, '/'),
         ]
         connection.query(sql, values, (err, result) => {
             if (err) throw err
