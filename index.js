cdbd34cdf0b4d181b4949667272c461da81e95aa
Claire Kodia <claire.kodia@gmail.com>
2020-05-22 15:21:38 +0200

    Enable calls from netlify frontend

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const register = require("./src/routes/register")
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: 'https://wild-thunder.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
